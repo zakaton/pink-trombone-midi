28aaf608b759e4cc2365677db5e11a70ca73a5cb
Zack <zack@ukaton.com>
2022-08-21 12:08:00 -0700

    formatting

--- src/pink-trombone/audio/nodes/noise/AudioNode.js
+++ src/pink-trombone/audio/nodes/noise/AudioNode.js
@@ -5,22 +5,26 @@
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-window.AudioContext.prototype.createNoise = function() {
-    const noiseNode = this.createBufferSource();
+window.AudioContext.prototype.createNoise = function () {
+  const noiseNode = this.createBufferSource();
 
-    const seconds = 1;
+  const seconds = 1;
 
-    const buffer = this.createBuffer(1, seconds*this.sampleRate, this.sampleRate);
-    const bufferChannel = buffer.getChannelData(0);
-    for(let sampleIndex = 0; sampleIndex < bufferChannel.length; sampleIndex++)
-        bufferChannel[sampleIndex] = ((Math.random(0) *2) -1);
+  const buffer = this.createBuffer(
+    1,
+    seconds * this.sampleRate,
+    this.sampleRate
+  );
+  const bufferChannel = buffer.getChannelData(0);
+  for (let sampleIndex = 0; sampleIndex < bufferChannel.length; sampleIndex++)
+    bufferChannel[sampleIndex] = Math.random(0) * 2 - 1;
 
-    noiseNode.buffer = buffer;
-    noiseNode.loop = true;
+  noiseNode.buffer = buffer;
+  noiseNode.loop = true;
 
-    noiseNode.start();
-    
-    return noiseNode;
-}
+  noiseNode.start();
+
+  return noiseNode;
+};
 
 export {};
